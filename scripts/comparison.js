cf873bad34ef28c20fdca95655cc738287d80a9e
karim khaled <karim.a.s.khaled@gmail.com>
2023-05-14 09:52:26 +0300

    structuring files + adding navbar

--- scripts/comparison.js
+++ scripts/comparison.js
@@ -5,7 +5,10 @@ let firstRepoCounter = 0
 let secondRepoCounter = 0
 
 FetchBtn.onclick = function(){
-  if(repoOneInput.value =='' || repoTwoInput.value == ''){alert('please fill the fields with valid info')}
+  if(repoOneInput.value =='' || repoTwoInput.value == ''){
+    alert('please fill the fields with valid info')
+  }
+
   else{
   // Start Repo One
   const API_REPO_ONE = `https://api.github.com/users/${repoOneInput.value}/repos`
@@ -13,6 +16,7 @@ FetchBtn.onclick = function(){
   fetch(API_REPO_ONE)
   .then((response) => {return response.json()})
   .then((dataOne)=>{
+    console.log(dataOne)
     for(let i = 0; i < dataOne.length; i++){
       firstRepoCounter++
     }
@@ -32,7 +36,10 @@ fetch(API_REPO_Two)
 // Finished Repo Two
 if(firstRepoCounter > secondRepoCounter){
   alert('First One Won')
-}else{alert('Second Repo Won')}
+  }else{alert('Second Repo Won')}
 }
 }
+
+
+
 
