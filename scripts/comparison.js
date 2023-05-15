ebdee6a115c7d8030688d0a5bc004046950da9ec
karim khaled <karim.a.s.khaled@gmail.com>
2023-05-15 21:39:26 +0300

    minor edits

--- scripts/comparison.js
+++ scripts/comparison.js
@@ -2,8 +2,6 @@ let repoOneInput = document.querySelector('.first-repo-inp')
 let repoTwoInput = document.querySelector('.second-repo-inp')
 let FetchBtn = document.querySelector('.btn')
 let theContainer = document.querySelector('.container')
-let firstRepoCounter = 0
-let secondRepoCounter = 0
 
 FetchBtn.onclick = function(){
   if(repoOneInput.value =='' || repoTwoInput.value == ''){
@@ -15,50 +13,32 @@ FetchBtn.onclick = function(){
   else{
   // Start Repo One
   const API_REPO_ONE = `https://api.github.com/users/${repoOneInput.value}/repos`
+  const API_REPO_TWO = `https://api.github.com/users/${repoTwoInput.value}/repos`
+  const API_REPO_ONE_AVATAR = `https://api.github.com/users/${repoOneInput.value}`
+  const API_REPO_TWO_AVATAR = `https://api.github.com/users/${repoTwoInput.value}`
 
-  fetch(API_REPO_ONE)
-    .then((response) => {return response.json()})
-    .then((dataOne)=>{
-      let avatarOne = dataOne.avatar_url
-      for(let i = 0; i < dataOne.length; i++){
-        firstRepoCounter++
+  fetch(API_REPO_ONE).then((response) => {return response.json()}).then((dataOne)=>{
+  fetch(API_REPO_ONE_AVATAR).then((response)=>{return response.json()}).then((dataOneImage)=>{let avatarOne = dataOneImage.avatar_url
+  fetch(API_REPO_TWO).then((response) => {return response.json()}).then((dataTwo)=>{
+  fetch(API_REPO_TWO_AVATAR).then((response)=>{return response.json()}).then((dataTwoImage)=>{let avatarTwo = dataTwoImage.avatar_url
+    if(dataOne.length > dataTwo.length ){
+      theContainer.innerHTML = 
+      `
+        <h1>The Winner is:</h1>
+        <h3>${repoOneInput.value}</h3>
+        <img src="${avatarOne}" alt="">
+      `
       }
-
-  const API_REPO_Two = `https://api.github.com/users/${repoTwoInput.value}/repos`
-
-  fetch(API_REPO_Two)
-    .then((response) => {return response.json()})
-    .then((dataTwo)=>{
-      let avatarTwo = dataTwo.avatar_url
-      for(let i = 0; i < dataTwo.length; i++){secondRepoCounter++}
-      if(firstRepoCounter > secondRepoCounter ){
-        theContainer.innerHTML = 
-        `
-          <h1>The Winner is:</h1>
-          <h3>${repoOneInput.value}</h3>
-          <img src="${avatarOne}" alt="">
-        `
-      }
-      else{
-        theContainer.innerHTML = 
-        `
-          <h1>The Winner is:</h1>
-          <h3>${repoTwoInput.value}</h3>
-          <img src="${avatarTwo}" alt="">
-        `
+    else{
+      theContainer.innerHTML = 
+      `
+        <h1>The Winner is:</h1>
+        <h3>${repoTwoInput.value}</h3>
+        <img src="${avatarTwo}" alt="">
+      `
           }
+        })
       })
-  }) 
-}
-}
-
-
-
-
-
-
-
-    // Finished Repo Two
-  //   if(dataOne.length === dataTwo.length){
-  //     alert('Draw')
-  // }
+    })
+  })
+}}
