6470d8cc589c33957a8b2c7df14eee1fe7c1622a
karim khaled <karim.a.s.khaled@gmail.com>
2023-05-15 09:35:14 +0300

    fixing repos page

--- scripts/repos.js
+++ scripts/repos.js
@@ -1,27 +1,60 @@
-let searchUserBtn = document.querySelector('.search-the-repos')
-let getRepos = document.querySelector('.searchbar')
-let reposDiv = document.querySelector('.repos')
-let reposDivTitle = document.querySelector('.title')
+// Main Variables
+let theInput = document.querySelector(".get-repos input");
+let getButton = document.querySelector(".get-button");
+let container =document.querySelector(".container");
+let profile = document.querySelector(".profile");
+let tabs = document.querySelector(".tabs");
+let repos = document.querySelector(".repos");
 
-searchUserBtn.onclick = function() {
-  event.preventDefault()
-  const API_REPO = `https://api.github.com/users/${getRepos.value}/repos`
-  let title = document.createElement('h1')
-  title.innerHTML = `<h1>${getRepos.value} Repos</h1> <hr>`
-  reposDivTitle.appendChild(title)
-  fetch(API_REPO)
-    .then((response) => {return response.json()})
-    .then((data) => {
-      for(let i = 0; i < data.length; i++){
-        let repo = document.createElement('div')
-        repo.innerHTML =
-        `
-          <h6>${data[i].name}</h6>
-          <p>HTML, CSS & Javascript</p>
-        `
-        reposDiv.appendChild(repo)
-        repo.className = 'repo'
-      }  
-    })   
-}
+
+getButton.onclick = function () {
+  getRepos();
+};
+
+// Get Repos Function
+function getRepos() {
+
+  if (theInput.value == "") {
+
+    alert("Enter username");
+
+  } else if(theInput.value) {
+
+    // to prevent form value
+    event.preventDefault()
+    fetch(`https://api.github.com/users/${theInput.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+
+        const avatarUrl = data.avatar_url;
+            profile.innerHTML = `
+            <a href="#"><img src="${avatarUrl}" alt="" class="profile-pic"></a>
+                <p>${theInput.value}</p>
+                <button id="profile-btn">Edit profile</button>`;
+            tabs.innerHTML = `
+            <a href="#">Overview</a>
+            <a href="#">Repositories</a>
+            <a href="#">Projects</a>
+            <a href="#">Packages</a>
+            <a href="#">Stars</a>`;
+
+      })
+      fetch(`https://api.github.com/users/${theInput.value}/repos`)
+      .then((response)=> response.json())
+      .then((repositories)=>{
+        for (let i = 0; i < repositories.length; i++){
+          let repo = document.createElement('div')
+          repo.className = "repo";
+          repo.innerHTML = `
+          <a href="#">${repositories[i].name}</a>
+          <p class="public-word">Public</p>
+          <p class="type-file">${repositories[i].language}</p>
+          `;
+          tabs.style.paddingBottom = "-80px";
+          repos.appendChild(repo)
+        }
+      })
+
+    }}
+
 
