e474611b7a26fe9f2517791c908c474b199036ff
karim khaled <karim.a.s.khaled@gmail.com>
2023-05-15 09:14:14 +0300

    fixed comaprison bug

--- scripts/comparison.js
+++ scripts/comparison.js
@@ -4,7 +4,7 @@ let FetchBtn = document.querySelector('.btn')
 let theContainer = document.querySelector('.container')
 let firstRepoCounter = 0
 let secondRepoCounter = 0
-var x,y
+
 FetchBtn.onclick = function(){
   if(repoOneInput.value =='' || repoTwoInput.value == ''){
     alert('please fill the fields with valid info')
@@ -14,54 +14,51 @@ FetchBtn.onclick = function(){
   }
   else{
   // Start Repo One
-  const API_REPO_ONE = `https://api.github.com/users/${repoOneInput.value}`
+  const API_REPO_ONE = `https://api.github.com/users/${repoOneInput.value}/repos`
 
   fetch(API_REPO_ONE)
-  .then((response) => {return response.json()})
-  .then((dataOne)=>{
-    var avatarOne = dataOne.avatar_url
+    .then((response) => {return response.json()})
+    .then((dataOne)=>{
+      let avatarOne = dataOne.avatar_url
+      for(let i = 0; i < dataOne.length; i++){
+        firstRepoCounter++
+      }
 
-    console.log(dataOne)
-    for(let i = 0; i < dataOne.length; i++){
-      firstRepoCounter++
-    }
+  const API_REPO_Two = `https://api.github.com/users/${repoTwoInput.value}/repos`
 
-// Finished Repo One
+  fetch(API_REPO_Two)
+    .then((response) => {return response.json()})
+    .then((dataTwo)=>{
+      let avatarTwo = dataTwo.avatar_url
+      for(let i = 0; i < dataTwo.length; i++){secondRepoCounter++}
+      if(firstRepoCounter > secondRepoCounter ){
+        theContainer.innerHTML = 
+        `
+          <h1>The Winner is:</h1>
+          <h3>${repoOneInput.value}</h3>
+          <img src="${avatarOne}" alt="">
+        `
+      }
+      else{
+        theContainer.innerHTML = 
+        `
+          <h1>The Winner is:</h1>
+          <h3>${repoTwoInput.value}</h3>
+          <img src="${avatarTwo}" alt="">
+        `
+          }
+      })
+  }) 
+}
+}
 
-// Start Repo Two
-const API_REPO_Two = `https://api.github.com/users/${repoTwoInput.value}`
 
-fetch(API_REPO_Two)
-.then((response) => {return response.json()})
-.then((dataTwo)=>{
-  var avatarTwo = dataTwo.avatar_url
-  for(let i = 0; i < dataTwo.length; i++){
-    secondRepoCounter++
-  }
+
+
+
+
+
     // Finished Repo Two
   //   if(dataOne.length === dataTwo.length){
   //     alert('Draw')
   // }
-    if(firstRepoCounter > secondRepoCounter ){
-      theContainer.innerHTML = 
-      `
-        <h1>The Winner is:</h1>
-        <h3>${repoOneInput.value}</h3>
-        img src="${avatarOne}" alt="">
-      `
-      }
-      else{
-      theContainer.innerHTML = `
-      <h1>The Winner is:</h1>
-      <h3>${repoTwoInput.value}</h3>
-      <img src="${avatarTwo}" alt="">
-      `
-      }
-    })
-  })
-}
-}
-
-
-
-
